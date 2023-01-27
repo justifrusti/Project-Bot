5bc1867bb01ac7a26c19c78652ac39bfdf16da4a
justifrusti <70446225+justifrusti@users.noreply.github.com>
2023-01-27 10:23:35 +0100

    Music Switcher Script

---
+++ Project Bot/Assets/Scripts/Gameplay/AreaMusicSwitcher.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaMusicSwitcher : MonoBehaviour
+{
+    public AudioSource source;
+
+    public AudioClip clipToPlay;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            source.clip = clipToPlay;
+
+            source.Play();
+            source.loop = true;
+        }
+    }
+}
