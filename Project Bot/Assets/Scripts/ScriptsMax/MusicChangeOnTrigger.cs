884bd83d5785ed5af9e243f4178312aed8cd309d
MrMaxico <mr.maxico4@gmail.com>
2023-01-27 06:16:34 +0100

    factory bijna klaar

--- Project Bot/Assets/Scripts/ScriptsMax/MusicChangeOnTrigger.cs
+++ Project Bot/Assets/Scripts/ScriptsMax/MusicChangeOnTrigger.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class MusicChangeOnTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        FindObjectOfType<AudioManagerScript>().StopPlaying("GameplayMusic");
+        FindObjectOfType<AudioManagerScript>().Play("naamvangeluidhier");
     }
 }
